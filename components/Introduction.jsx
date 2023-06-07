c8653c1f5c9103f0e618592e3ee32a9916cfe8ec
MohdBasurra <mohdye96@gmail.com>
2023-06-08 00:27:13 +0300

    fix verecl Error

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -4,7 +4,7 @@ const Introduction = () => {
     return (  <>
    <div className='flex flex-col justify-center items-center p-2 text-center'>
   <h1 className='text-2xl'>Welcome To Creality YEMEN</h1>
-  <p className='text-xl'>Creality YE is an official distributor for Creality and Ender products here in YEMEN. Any inquiry, don't hesitate to Contact with us</p>
+  <p className='text-xl'>Creality YE is an official distributor for Creality and Ender products here in YEMEN. Any inquiry, dont hesitate to Contact with us</p>
   <div className='flex w-full justify-center'>
     <div className='p-4'>
       <Button color={'custom-blue'} text={'Our products'}/>
