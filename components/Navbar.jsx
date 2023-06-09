4573248037018dd39682b5d0df49be4351a95b6c
MohdBasurra <mohdye96@gmail.com>
2023-06-10 01:11:15 +0300

    fix the search bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -40,42 +40,29 @@ function Header() {
 
   return (<>
 
-<div class="bg-white pt-4">
-  <div class="container mx-auto flex  items-star justify-start md:justify-between ">
+<div class="pt-4 mx-auto ">
+  <div class="container   mx-auto flex items-center justify-between ">
 
     {/* <!-- logo --> */}
-    <div class="  md:w-48 ">
+    <div class="  md:w-48  pr-2 ">
     <Image src={crealityLogo} alt="Creality Logo" width={280} height={250} />    </div>
-
+ 
     {/* <!-- search --> */}
-    <div  class=" flex flex-wrap   bg-blue-600  rounded-md md:hidden xl:flex items-center justify-center">
-      <select class="bg-transparent uppercase font-bold text-sm p-4 mr-4 " name="" id="">
-        <option>All categories</option> 
-        <option>Filament</option>
-        <option >Resin</option>
-        <option>3D printers</option>
-        <option>Spare parts</option>
-      </select>
+    <div className='flex justify-end items-end   mx-auto bg-gray-100'>
+    <div  class="  rounded-md   xl:flex items-center justify-center hidden md:block  ">
+      
       <input class="  bg-transparent font-semibold text-xl pl-4" type="text" placeholder="I'm searching for ..."/>
       {/* Seacrch Icon */}
-      {/* <svg class="ml-auto h-5 px-4 text-gray-500" aria-hidden="true" focusable="false" data-prefix="far" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-16 fa-9x"><path fill="currentColor" d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"></path></svg> */}
-    </div>
+       <svg class="ml-auto h-5 px-4 text-gray-500" aria-hidden="true" focusable="false" data-prefix="far" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"   > <path fill="currentColor" d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"></path></svg> 
+    </div></div>
 
     {/* <!-- phone number --> */}
-    <div class="ml-auto md:w-48 hidden sm:flex flex-col place-items-end ">
-      <span class="font-bold md:text-xl">738 074 693</span>
-      <span class="font-semibold text-sm text-gray-400">Support 24/7</span>
-    </div>
+   
 
     {/* <!-- buttons --> */}
-    <nav class="contents bg-gray-500">
+    <nav class="contents     flex-wrap">
       <ul class="ml-4 xl:w-48 flex items-center justify-end">
-        <li class="ml-2 lg:ml-4 relative inline-block">
-          <a class="" href="">
-            <svg class="h-9 lg:h-10 p-2 text-gray-500" aria-hidden="true" focusable="false" data-prefix="far" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" >
-              <path fill="currentColor" d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"></path></svg>
-          </a>
-        </li>
+        
         <li class="ml-2 lg:ml-4 relative inline-block">
           <a class="" href="">
             <div class="absolute -top-1 right-0 z-10 bg-custom-blue text-xs font-bold px-1 py-0.5 rounded-sm">3</div>
