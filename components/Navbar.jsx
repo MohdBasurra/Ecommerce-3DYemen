54ee268ecb135fae2295bf1255277f45fc5ebb94
MohdBasurra <mohdye96@gmail.com>
2023-07-02 02:11:31 +0300

    fix navbar code

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const navigation = [
   { name: "Home", href: "/", current: false },
@@ -81,7 +82,7 @@ function Header() {
           <nav class="contents     flex-wrap">
             <ul class="ml-4 xl:w-48 flex items-center justify-end">
               <li class="ml-2 lg:ml-4 relative inline-block">
-                <a class="" href="">
+                <Link class="" href="">
                   <div class="absolute -top-1 right-0 z-10 bg-custom-blue text-xs font-bold px-1 py-0.5 rounded-sm">
                     3
                   </div>
@@ -100,10 +101,10 @@ function Header() {
                       d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"
                     ></path>
                   </svg>
-                </a>
+                </Link>
               </li>
               <li class="ml-2 lg:ml-4 relative inline-block">
-                <a class="" href="/cart">
+                <Link class="" href="/cart">
                   <div class="absolute -top-1 right-0 z-10 bg-custom-blue text-xs font-bold px-1 py-0.5 rounded-sm">
                     7
                   </div>
@@ -122,7 +123,7 @@ function Header() {
                       d="M551.991 64H144.28l-8.726-44.608C133.35 8.128 123.478 0 112 0H12C5.373 0 0 5.373 0 12v24c0 6.627 5.373 12 12 12h80.24l69.594 355.701C150.796 415.201 144 430.802 144 448c0 35.346 28.654 64 64 64s64-28.654 64-64a63.681 63.681 0 0 0-8.583-32h145.167a63.681 63.681 0 0 0-8.583 32c0 35.346 28.654 64 64 64 35.346 0 64-28.654 64-64 0-18.136-7.556-34.496-19.676-46.142l1.035-4.757c3.254-14.96-8.142-29.101-23.452-29.101H203.76l-9.39-48h312.405c11.29 0 21.054-7.869 23.452-18.902l45.216-208C578.695 78.139 567.299 64 551.991 64zM208 472c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm256 0c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm23.438-200H184.98l-31.31-160h368.548l-34.78 160z"
                     ></path>
                   </svg>
-                </a>
+                </Link>
               </li>
             </ul>
           </nav>
@@ -161,7 +162,7 @@ function Header() {
                     <div className="hidden sm:ml-6 sm:block">
                       <div className="flex flex-wrap space-x-4">
                         {navigation.map((item) => (
-                          <a
+                          <Link
                             key={item.name}
                             href={item.href}
                             className={classNames(
@@ -173,7 +174,7 @@ function Header() {
                             aria-current={item.current ? "page" : undefined}
                           >
                             {item.name}
-                          </a>
+                          </Link>
                         ))}
                       </div>
                     </div>
@@ -203,7 +204,7 @@ function Header() {
                         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                           <Menu.Item>
                             {({ active }) => (
-                              <a
+                              <Link
                                 href="#"
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
@@ -211,12 +212,12 @@ function Header() {
                                 )}
                               >
                                 Your Profile
-                              </a>
+                              </Link>
                             )}
                           </Menu.Item>
                           <Menu.Item>
                             {({ active }) => (
-                              <a
+                              <Link
                                 href="#"
                                 className={classNames(
                                   active ? "bg-gray-100" : "",
@@ -224,12 +225,12 @@ function Header() {
                                 )}
                               >
                                 Settings
-                              </a>
+                              </Link>
                             )}
                           </Menu.Item>
                           <Menu.Item>
                             {({ active }) => (
-                              <a
+                              <Link
                                 href="#"
                                 className={classNames(
                                   active ? "bg-gray-100 text-red-600" : "",
@@ -237,7 +238,7 @@ function Header() {
                                 )}
                               >
                                 Sign out
-                              </a>
+                              </Link>
                             )}
                           </Menu.Item>
                         </Menu.Items>
